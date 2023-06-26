771f4559b0e5ec72ef91d05e37be2b48c94f84da
oikkoikk <ljjun0702@gmail.com>
2023-06-26 22:53:42 +0900

    [SWM-90] Add : google login button

--- src/components/heading/MainDescription.tsx
+++
@@ -1,3 +0,0 @@
-export default function MainDescription({ content }: { content: string }) {
-  return <h3 className='mt-5 ml-16 text-3xl font-semibold'>{content}</h3>;
-}
--- src/components/heading/MainHeading.tsx
+++
@@ -1,3 +0,0 @@
-export default function MainHeading({ content }: { content: string }) {
-  return <h2 className='mt-24 ml-16 text-5xl font-bold mb-14'>{content}</h2>;
-}
