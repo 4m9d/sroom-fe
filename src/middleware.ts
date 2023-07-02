9d2e24a2d82f72739b1c5bc3aa212e8a19c0ac8d
oikkoikk <ljjun0702@gmail.com>
2023-07-02 12:20:16 +0900

    [SWM-91] Feat : middleware route protection

---
+++ src/middleware.ts
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { getToken } from 'next-auth/jwt';
+
+const secret = process.env.NEXTAUTH_SECRET;
+
+export async function middleware(req: NextRequest) {
+  const session = await getToken({ req, secret, raw: true });
+  const { pathname } = req.nextUrl;
+
+  if (pathname === '/' && session) {
+    return NextResponse.redirect(new URL('/dashboards', req.url));
+  } else if (pathname.length > 1 && !session) {
+    return NextResponse.redirect(new URL('/', req.url));
+  }
+}
+
+export const config = {
+  matcher: ['/', '/dashboards']
+};
