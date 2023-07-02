9448fa668575d29b47d089d317681966be34053a
oikkoikk <ljjun0702@gmail.com>
2023-07-02 23:31:51 +0900

    [SWM-91] Feat : log out

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,13 @@
+type Props = {
+  className?: string;
+  children: React.ReactNode;
+  onClick?: () => void;
+};
+
+export default function Button({ className, children, onClick }: Props) {
+  return (
+    <button onClick={onClick} className={`${className} btn btn-ghost`}>
+      {children}
+    </button>
+  );
+}
