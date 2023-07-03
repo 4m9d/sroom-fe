d466e7e60fb836a77d1d579b8f93e0b6eb6dae12
oikkoikk <ljjun0702@gmail.com>
2023-07-04 00:27:22 +0900

    [SWM-91] Feat : login / logout error handling

--- src/hook/useAuth.ts
+++ src/hook/useAuth.ts
@@ -3,15 +3,24 @@ import { useRouter } from 'next/navigation';
 import { fetchUserAuthWithRefreshToken } from '../api/auth/login';
 import { useQuery } from '@tanstack/react-query';
 import { QueryKeys } from '../lib/queryKeys';
+import useAlert from './useAlert';
 
 export default function useAuth() {
   const router = useRouter();
   const { data: session, status, update } = useSession();
+  const { setAlert } = useAlert();
   const NOW = Math.floor(Date.now() / 1000);
   //서버에서 설정한 만료 시간보다 1분 짧게 변경
   const REFRESH_PERIOD = session ? session?.expiresAt - NOW - 60 * 1000 : 0;
   const refreshToken = { refreshToken: session?.refreshToken ?? '' };
 
+  const loginErrorHandler = (e: Error) => {
+    setAlert({
+      type: 'error',
+      title: '에러 발생',
+      description: e.message
+    });
+  };
   const silentRefresh = async (refreshToken: RefreshToken, update: any) => {
     const response = await fetchUserAuthWithRefreshToken(refreshToken);
     if (response.ok) {
@@ -36,16 +45,21 @@ export default function useAuth() {
   const login = async (credential: GoogleLoginResponse) => {
     //TODO: 로그 추후 삭제
     console.log('구글 토큰', credential);
+    if (!credential) return;
+
     await signIn('credentials', {
       credential: credential,
       redirect: false
-    }).then((res) => {
-      if (res?.ok) {
+    })
+      .then((res) => {
+        if (!res?.ok) {
+          throw new Error('로그인에 실패했어요');
+        }
         router.replace('/dashboards');
-      } else {
-        console.log('로그인 실패');
-      }
-    });
+      })
+      .catch((err) => {
+        loginErrorHandler(err);
+      });
   };
 
   return { session, status, login, silentRefresh };
