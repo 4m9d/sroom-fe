b3919810093908760fa11ea0fd17ea7e51b98d4a
oikkoikk <ljjun0702@gmail.com>
2023-07-09 14:10:05 +0900

    [SWM-147] Add : Toast animation with framer-motion

--- src/components/ui/Toast.tsx
+++ src/components/ui/Toast.tsx
@@ -1,39 +1,12 @@
 'use client';
 import { ToastContext } from '@/src/lib/ToastProvider';
+import { AnimatePresence, motion } from 'framer-motion';
 import { useContext } from 'react';
 
-const SVG = {
-  success: (
-    <svg
-      xmlns='http://www.w3.org/2000/svg'
-      className='w-6 h-6 stroke-current shrink-0'
-      fill='none'
-      viewBox='0 0 24 24'
-    >
-      <path
-        strokeLinecap='round'
-        strokeLinejoin='round'
-        strokeWidth='2'
-        d='M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z'
-      />
-    </svg>
-  ),
-  error: (
-    <svg
-      xmlns='http://www.w3.org/2000/svg'
-      className='w-6 h-6 stroke-current shrink-0'
-      fill='none'
-      viewBox='0 0 24 24'
-    >
-      <path
-        strokeLinecap='round'
-        strokeLinejoin='round'
-        strokeWidth='2'
-        d='M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z'
-      />
-    </svg>
-  )
-};
+const Emoji = {
+  success: '✅',
+  error: '🚫'
+} as const;
 
 export default function Toast() {
   const { toasts } = useContext(ToastContext);
@@ -45,21 +18,26 @@ export default function Toast() {
   const { type, title, description, buttonLabel, buttonOnClick } = toast;
 
   return (
-    <div
-      className={`alert ${
-        type === 'error' ? 'alert-error' : 'alert-success'
-      } absolute z-10 bottom-16 w-1/2 left-1/4`}
-    >
-      <span className='inline-block mr-2 align-middle'>{SVG[type]}</span>
-      <div>
-        <h3 className='font-bold'>{title}</h3>
-        <p className='text-xs'>{description}</p>
-      </div>
-      {buttonLabel && buttonOnClick && (
-        <button onClick={buttonOnClick} className='btn btn-sm btn-ghost'>
-          {buttonLabel}
-        </button>
-      )}
-    </div>
+    <AnimatePresence>
+      <motion.div
+        initial={{ y: 200 }}
+        animate={{ y: 0 }}
+        transition={{ repeat: 1, repeatType: 'reverse', repeatDelay: 3 }}
+        className={`alert h-14 flex items-center ${
+          type === 'error' ? 'alert-error' : 'alert-success'
+        } absolute z-10 bottom-16 w-1/2 left-1/4`}
+      >
+        <span className='inline-block mr-2 align-middle'>{Emoji[type]}</span>
+        <div>
+          <h3 className='font-bold'>{title}</h3>
+          <p className='text-xs'>{description}</p>
+        </div>
+        {buttonLabel && buttonOnClick && (
+          <button onClick={buttonOnClick} className='btn btn-sm btn-ghost'>
+            {buttonLabel}
+          </button>
+        )}
+      </motion.div>
+    </AnimatePresence>
   );
 }
