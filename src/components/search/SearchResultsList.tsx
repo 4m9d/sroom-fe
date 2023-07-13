497305c4a3c9677b8ed098b86664520dcb70bc4b
oikkoikk <ljjun0702@gmail.com>
2023-07-14 00:44:14 +0900

    [SWM-171] Feat : search lecture card component

--- src/components/search/SearchResultsList.tsx
+++ src/components/search/SearchResultsList.tsx
@@ -1,25 +1,47 @@
 'use client';
+import { ErrorMessage } from '@/src/api/ErrorMessage';
 import { fetchLecturesByKeyword } from '@/src/api/lectures/search';
 import { QueryKeys } from '@/src/api/queryKeys';
 import useToast from '@/src/hooks/useToast';
 import { useQuery } from '@tanstack/react-query';
+import SearchLectureCard from '../ui/SearchLectureCard';
+import { GRID_COLS_2 } from '@/src/constants/ui/searchLectureCard';
+import { CACHE_TIME, STALE_TIME } from '@/src/constants/search/search';
+
+const queryOptions = {
+  refetchOnWindowFocus: false,
+  suspense: true,
+  staleTime: STALE_TIME,
+  cacheTime: CACHE_TIME
+};
 
 export default async function SearchResultsList(
   requestParam: SearchLectureParams
 ) {
   const fetchSearchResults = async () => {
-    console.log('hihi')
     return await fetchLecturesByKeyword(requestParam)
       .then((res) => {
-        return res as SearchResultsList;
+        return res;
       })
-      .catch((err) => {
-        errorHandler(err);
+      .catch(() => {
+        setErrorToast(new Error(ErrorMessage.search));
         return null;
       });
-  }
-  const results = useQuery([QueryKeys.SEARCH], fetchSearchResults);
-  const { errorHandler } = useToast();
+  };
+  const { setErrorToast } = useToast();
+  const { data } = useQuery(
+    [QueryKeys.SEARCH, requestParam.keyword],
+    fetchSearchResults,
+    queryOptions
+  );
 
-  return <div>hihi</div>;
+  return (
+    <>
+      <ul className={GRID_COLS_2}>
+        {data?.lectures.map((lecture: Lecture) => (
+          <SearchLectureCard key={lecture.lectureCode} lecture={lecture} />
+        ))}
+      </ul>
+    </>
+  );
 }
