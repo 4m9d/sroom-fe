60c245cf25f5641d1d735de7ae45dc42ae6a9eb2
oikkoikk <ljjun0702@gmail.com>
2023-07-14 17:18:10 +0900

    [SWM-175] Feat : search results filter

--- src/components/search/SearchResultsHeading.tsx
+++ src/components/search/SearchResultsHeading.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export default function SearchResultsHeading({ keyword }: Props) {
   return (
-    <h2 className='mt-10 mb-20 ml-10 text-4xl'>
+    <h2 className='mt-10 mb-10 ml-10 text-4xl'>
       <span className='h-16'>{"'"}</span>
       <span className='inline-block truncate max-w-1/2'>{keyword}</span>
       <span>{"'"}</span>
