334a42cea7cdcbf956e07d5ffb7014c937c7deaa
oikkoikk <ljjun0702@gmail.com>
2023-07-19 00:49:50 +0900

    [SWM-181] Feat : Lecture Detail Index List

---
+++ src/components/lectureDetail/index/LectureDetailIndexSkeleton.tsx
@@ -0,0 +1,31 @@
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
+
+type Props = {
+  limit: number;
+};
+
+export default function LectureDetailIndexSkeleton({ limit }: Props) {
+  const skeletonArray = [...new Array(limit)].map((_, i) => i + 1);
+
+  const StyledSkeleton = () => {
+    return (
+      <li
+        className={
+          'flex items-center justify-between h-20 w-full px-4'
+        }
+      >
+        <div className='w-full h-full'>
+          <Skeleton height='100%' />
+        </div>
+      </li>
+    );
+  };
+  return (
+    <ul className='grid grid-cols-1 gap-y-4'>
+      {skeletonArray.map((idx) => (
+        <StyledSkeleton key={'skeleton' + idx} />
+      ))}
+    </ul>
+  );
+}
