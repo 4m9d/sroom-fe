bcdd2500b879d533617351c26823e5aba39bfe48
oikkoikk <ljjun0702@gmail.com>
2023-07-19 00:48:31 +0900

    [SWM-181] Fix : no SSR in search/[lecture_code]

--- src/app/search/[lecture_code]/page.tsx
+++ src/app/search/[lecture_code]/page.tsx
@@ -1,14 +1,24 @@
+'use client';
 import { fetchLectureDetail } from '@/src/api/lectures/search';
-import LectureDetailModal from '@/src/components/lectureDetail/LectureDetailModal';
+import dynamic from 'next/dynamic';
+
+const DynamicLectureDetailModal = dynamic(
+  () => import('@/src/components/lectureDetail/LectureDetailModal'),
+  { ssr: false }
+);
 
 export default async function LectureDetail({
   params: { lecture_code }
 }: LectureDetailModalParams) {
-  const lectureDetail = await fetchLectureDetail(lecture_code)
-    .then((res) => res)
+  const lectureDetail = await fetchLectureDetail(lecture_code).then(
+    (res) => res
+  );
 
   return (
-    <LectureDetailModal lectureDetail={lectureDetail} navigationType='hard' />
+    <DynamicLectureDetailModal
+      lectureDetail={lectureDetail}
+      navigationType='hard'
+    />
   );
 }
 //NOTE: 수정하면, @modal/(.)search/[lectureCode]/page 에서도 수정해야함
