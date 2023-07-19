d7fc439d1bca2ae5c35394c5b12abc78e6efb7b1
oikkoikk <ljjun0702@gmail.com>
2023-07-20 02:22:40 +0900

    [SWM-181] Feat : modal initial animation

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -1,23 +1,34 @@
+import { AnimatePresence, motion } from 'framer-motion';
+
 type Props = {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  onClose?: () => void;
+  onClose: () => void;
 };
 
 export default function Modal({ id, className, children, onClose }: Props) {
   return (
-    <div className={'modal modal-open'}>
-      <div onClick={onClose} className="modal-backdrop"/>
-      <div id={id} className={`px-12 py-7 modal-box min-w-[70vw] ${className}`}>
-        <button
-          onClick={onClose}
-          className='absolute btn btn-sm btn-circle btn-ghost right-2 top-2'
+    <AnimatePresence>
+      <div className={'modal modal-open'}>
+        <div onClick={onClose} className='modal-backdrop' />
+        <motion.div
+          id={id}
+          key={id}
+          initial={{ opacity: 0, scale: 0.97 }}
+          animate={{ opacity: 1, scale: 1 }}
+          exit={{ opacity: 0, scale: 0.97 }}
+          className={`px-12 py-7 modal-box min-w-[70vw] ${className}`}
         >
-          ✕
-        </button>
-        {children}
-      d</div>
-    </div>
+          <button
+            onClick={onClose}
+            className='absolute btn btn-sm btn-circle btn-ghost right-2 top-2'
+          >
+            ✕
+          </button>
+          {children}
+        </motion.div>
+      </div>
+    </AnimatePresence>
   );
 }
