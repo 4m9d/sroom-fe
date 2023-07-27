90379b5ec53a28fbaa788440acbb7b8b93a3c6d1
oikkoikk <ljjun0702@gmail.com>
2023-07-27 21:52:56 +0900

    [SWM-207] Feat : completed / total video count in LatestLearningLectureCard

--- src/components/dashboard/latestLearning/LatestLearningLectureCard.tsx
+++ src/components/dashboard/latestLearning/LatestLearningLectureCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import ProgressBar from '../../ui/ProgressBar';
 import HorizontalSmallLectureCard from '../../ui/lectureCard/HorizontalSmallLectureCard';
 import getRelativeTime from '@/src/util/day/getRelativeTime';
@@ -6,9 +6,9 @@ import Button from '../../ui/Button';
 
 type Props = {
   lecture: LatestLearningLecture;
-}
+};
 
-export default function LatestLearningLectureCard({lecture}: Props) {
+export default function LatestLearningLectureCard({ lecture }: Props) {
   return (
     <HorizontalSmallLectureCard
       src={lecture.thumbnail}
@@ -24,15 +24,20 @@ export default function LatestLearningLectureCard({lecture}: Props) {
           </p>
         </div>
         <div className='flex justify-between h-10 gap-5 mb-1'>
-          <div className='flex flex-col justify-between flex-1 py-1'>
+          <div className='flex flex-col justify-between flex-1 gap-1 py-1'>
             <p className='text-xs text-zinc-500'>
-              총 강의 시간 : {lecture.course_duration}분 |{' '}
-              {getRelativeTime(lecture.last_view_time)}
+              총 강의 시간 : {lecture.duration}분 | 수강한 영상 :{' '}
+              {lecture.completed_video_count}개 / {lecture.total_video_count}개
             </p>
-            <ProgressBar
-              className='w-full h-[5px] bg-zinc-100'
-              value={lecture.progress}
-            />
+            <div className='flex items-center'>
+              <ProgressBar
+                className='w-full h-[5px] bg-zinc-100'
+                value={lecture.progress}
+              />
+              <p className='ml-2 text-xs font-semibold text-orange-500'>
+                {lecture.progress}%
+              </p>
+            </div>
           </div>
           <Button className='flex justify-between w-32 text-sm text-zinc-200 bg-zinc-800'>
             <p>바로 학습</p>
