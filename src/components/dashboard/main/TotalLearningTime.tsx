ca564a1a2f55071e1a0a21abce1d46198886e2bf
oikkoikk <ljjun0702@gmail.com>
2023-07-28 00:51:37 +0900

    [SWM-207] Fix : total learning time unit

--- src/components/dashboard/main/TotalLearningTime.tsx
+++ src/components/dashboard/main/TotalLearningTime.tsx
@@ -1,13 +1,34 @@
+import { useCallback, useMemo } from 'react';
+
 type Props = {
   time: number;
-}
+};
 
-export default function TotalLearningTime({time}: Props) {
+export default function TotalLearningTime({ time }: Props) {
+  const timeUnitHandler = useCallback((time: number) => {
+    const timeInString = time.toString();
+    let result = [];
+
+    if (timeInString.length > 3) {
+      result = [Math.floor(time / 60).toString(), '시간'];
+    } else {
+      result = [timeInString, '분'];
+    }
+
+    return result;
+  }, []);
+
+  const [timeInString, timeUnit] = useMemo(
+    () => timeUnitHandler(time),
+    [time, timeUnitHandler]
+  );
+
   return (
     <div className='flex flex-col justify-between col-start-1 col-end-2 row-start-2 row-end-4 px-5 py-5 bg-zinc-100'>
       <p className='text-sm'>누적 수강 시간</p>
-      <p className='text-xl'>
-        <span className='font-bold text-7xl'>{time}</span>시간
+      <p className='font-bold text-7xl'>
+        {timeInString}
+        <span className='text-lg font-semibold'>{timeUnit}</span>
       </p>
     </div>
   );
