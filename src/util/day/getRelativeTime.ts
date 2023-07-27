c1fe9f94e92eebe609201f2a9a077dde1e51118b
oikkoikk <ljjun0702@gmail.com>
2023-07-27 19:40:32 +0900

    [SWM-207] Feat : getRelativeTime util

---
+++ src/util/day/getRelativeTime.ts
@@ -0,0 +1,10 @@
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import 'dayjs/locale/ko';
+
+dayjs.locale('ko');
+dayjs.extend(relativeTime);
+
+export default function getRelativeTime(time: string) {
+  return dayjs(time).fromNow();
+}
