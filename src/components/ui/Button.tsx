45870b2c7f54725b2159e1a824cc3b27ab14c1e6
oikkoikk <ljjun0702@gmail.com>
2023-07-30 21:41:59 +0900

    [SWM-208] Fix : ui components' layout

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -6,7 +6,10 @@ type Props = {
 
 export default function Button({ className, children, onClick }: Props) {
   return (
-    <button onClick={onClick} className={`${className} py-1 px-4 flex hover:opacity-90 active:focus:scale-95 transition-all items-center justify-center rounded-none`}>
+    <button
+      onClick={onClick}
+      className={`${className} py-1 px-4 flex hover:opacity-90 active:focus:scale-95 transition-all items-center justify-center rounded-none`}
+    >
       {children}
     </button>
   );
--- src/components/ui/LoadMoreButton.tsx
+++ src/components/ui/LoadMoreButton.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Button from './Button';
 
 type Props = {
+  title: string;
   onClick: () => any;
 };
 
-export default function LoadMoreButton({ onClick }: Props) {
+export default function LoadMoreButton({ title, onClick }: Props) {
   return (
-    <Button onClick={onClick} className='mx-auto btn-md btn-wide'>
-      {'더보기'}
+    <Button onClick={onClick} className='mx-auto text-base btn-md btn-wide text-zinc-500'>
+      {title}
     </Button>
   );
 }
