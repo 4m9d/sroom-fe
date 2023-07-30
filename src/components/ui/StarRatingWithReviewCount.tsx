f83a52e66228dc3ebc09ae8ab2c2f2ca87507472
oikkoikk <ljjun0702@gmail.com>
2023-07-30 21:41:59 +0900

    [SWM-208] Fix : ui components' layout

--- src/components/ui/StarRatingWithReviewCount.tsx
+++ src/components/ui/StarRatingWithReviewCount.tsx
@@ -5,12 +5,12 @@ type Props = {
 
 export default function StarRatingWithReviewCount({rating, review_count}: Props) {
   return (
-    <div className='flex w-24'>
+    <div className='flex w-24 h-1'>
       <p className='mr-1 text-xs font-bold text-orange-500'>
         ★ {rating}
       </p>
       <p className='text-xs font-bold text-orange-500 underline'>
-        후기 {review_count}개
+        후기 {review_count.toLocaleString()}개
       </p>
     </div>
   );
