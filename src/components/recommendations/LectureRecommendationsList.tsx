1e4bb5f25b9cf6816776016c632522b4798d8553
oikkoikk <ljjun0702@gmail.com>
2023-07-30 23:17:09 +0900

    [SWM-280] Fix : hide scrollbar

--- src/components/recommendations/LectureRecommendationsList.tsx
+++ src/components/recommendations/LectureRecommendationsList.tsx
@@ -9,7 +9,7 @@ export default function LectureRecommendationsList({ recommendations }: Props) {
   return (
     <section className='relative px-20 py-20 mx-auto my-20 max-w-screen-2xl'>
       <SectionHeading title='이런 강의는 어때요?' />
-      <ul className='flex gap-8 overflow-x-scroll overflow-y-hidden overscroll-x-contain'>
+      <ul className='flex gap-8 overflow-x-scroll overflow-y-hidden overscroll-x-contain scrollbar-hide'>
         {recommendations.map((lecture) => (
           <li key={lecture.lecture_code}>
             <LectureRecommendationsCard lecture={lecture} />
