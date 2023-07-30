1ce5e1e803e6ec368a631d4a909eff85580d8b11
oikkoikk <ljjun0702@gmail.com>
2023-07-30 23:08:47 +0900

    [SWM-280] Fix : relative time format logic

--- src/util/day/getRelativeTime.ts
+++ src/util/day/getRelativeTime.ts
@@ -6,5 +6,24 @@ dayjs.locale('ko');
 dayjs.extend(relativeTime);
 
 export default function getRelativeTime(time: string) {
-  return dayjs(time).fromNow();
+  const format = {
+    year: dayjs(time).diff(dayjs(), 'year'),
+    month: dayjs(time).diff(dayjs(), 'month'),
+    day: dayjs(time).diff(dayjs(), 'day'),
+    hour: dayjs(time).diff(dayjs(), 'hour'),
+    minute: dayjs(time).diff(dayjs(), 'minute')
+  } as const;
+  const formatKeys = Object.keys(format);
+
+  for (let i = 0; i < formatKeys.length; i++) {
+    const currFormatKey = formatKeys[i] as keyof typeof format;
+    const currFormat = format[currFormatKey];
+
+    if (currFormat !== 0) {
+      return new Intl.RelativeTimeFormat('ko', {
+        style: 'long',
+        numeric: 'always'
+      }).format(currFormat, formatKeys[i] as Intl.RelativeTimeFormatUnit);
+    }
+  }
 }
