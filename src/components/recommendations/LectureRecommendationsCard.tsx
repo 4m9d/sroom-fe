7a12ec3cbd3980696a5ba9af3f8794e48b74acfa
oikkoikk <ljjun0702@gmail.com>
2023-07-30 21:50:56 +0900

    [SWM-208] Fix : lecture recommendations card badge

--- src/components/recommendations/LectureRecommendationsCard.tsx
+++ src/components/recommendations/LectureRecommendationsCard.tsx
@@ -1,4 +1,5 @@
 import StarRatingWithReviewCount from '../ui/StarRatingWithReviewCount';
+import ThumbnailBadge from '../ui/ThumbnailBadge';
 import VerticalSmallLectureCard from '../ui/lectureCard/VerticalSmallLectureCard';
 
 type Props = {
@@ -6,32 +7,44 @@ type Props = {
 };
 
 export default function LectureRecommendationsCard({ lecture }: Props) {
+  const {
+    lecture_title,
+    channel,
+    description,
+    rating,
+    review_count,
+    thumbnail,
+    is_playlist
+  } = lecture;
+
   return (
-    <VerticalSmallLectureCard
-      src={lecture.thumbnail}
-      alt={lecture.lecture_title}
-    >
+    <VerticalSmallLectureCard src={thumbnail} alt={lecture_title}>
       <div className='h-full'>
         <div className='mb-1'>
           <p className='text-lg font-bold whitespace-normal line-clamp-1'>
-            {lecture.lecture_title}
+            {lecture_title}
           </p>
         </div>
         <div className='mb-1'>
           <p className='text-sm font-semibold whitespace-normal text-zinc-700 line-clamp-1'>
-            {lecture.channel}
+            {channel}
           </p>
         </div>
         <div className='mb-1 min-h-[3.5rem]'>
           <p className='text-sm whitespace-normal text-zinc-500 line-clamp-3'>
-            {lecture.description}
+            {description}
           </p>
         </div>
         <StarRatingWithReviewCount
-          rating={lecture.rating}
-          review_count={lecture.review_count}
+          rating={rating}
+          review_count={review_count}
         />
       </div>
+      {is_playlist && (
+        <div className='absolute top-0 left-0'>
+          <ThumbnailBadge title='재생목록' className='bg-orange-500' />
+        </div>
+      )}
     </VerticalSmallLectureCard>
   );
 }
