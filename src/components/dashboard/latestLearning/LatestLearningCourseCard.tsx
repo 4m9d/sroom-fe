a6e47ee5a47dde80e707caacb0b17051adb5c15e
oikkoikk <ljjun0702@gmail.com>
2023-07-31 23:22:33 +0900

    [SWM-280] Rename : LatestLearningLectures to LatestLearningCourses

--- src/components/dashboard/latestLearning/LatestLearningCourseCard.tsx
+++ src/components/dashboard/latestLearning/LatestLearningCourseCard.tsx
@@ -4,37 +4,37 @@ import HorizontalSmallLectureCard from '../../ui/lectureCard/HorizontalSmallLect
 import Button from '../../ui/button/Button';
 
 type Props = {
-  lecture: LatestLearningLecture;
+  course: Course;
 };
 
-export default function LatestLearningLectureCard({ lecture }: Props) {
+export default function LatestLearningCourseCard({ course }: Props) {
   return (
     <HorizontalSmallLectureCard
-      src={lecture.thumbnail}
-      alt={lecture.course_title}
+      src={course.thumbnail}
+      alt={course.course_title}
     >
       <div className='flex flex-col justify-between h-full'>
         <div className='mt-2'>
           <p className='text-lg font-bold whitespace-normal line-clamp-2'>
-            {lecture.course_title}
+            {course.course_title}
           </p>
           <p className='text-sm font-semibold whitespace-normal text-zinc-500 line-clamp-1'>
-            {lecture.channels}
+            {course.channels}
           </p>
         </div>
         <div className='flex justify-between h-10 gap-5 mb-1'>
           <div className='flex flex-col justify-between flex-1 gap-1 py-1'>
             <p className='text-xs text-zinc-500'>
-              총 강의 시간 : {lecture.duration.toLocaleString()}분 | 수강한 영상 :{' '}
-              {lecture.completed_video_count.toLocaleString()}개 / {lecture.total_video_count.toLocaleString()}개
+              총 강의 시간 : {course.duration.toLocaleString()}분 | 수강한 영상 :{' '}
+              {course.completed_video_count.toLocaleString()}개 / {course.total_video_count.toLocaleString()}개
             </p>
             <div className='flex items-center'>
               <ProgressBar
                 className='w-full h-[5px] bg-zinc-100'
-                value={lecture.progress}
+                value={course.progress}
               />
               <p className='ml-2 text-xs font-semibold text-orange-500'>
-                {lecture.progress}%
+                {course.progress}%
               </p>
             </div>
           </div>
