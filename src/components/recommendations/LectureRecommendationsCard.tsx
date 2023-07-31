07214fcc0fa83d86bc756b29ed31e63d3144f2ca
oikkoikk <ljjun0702@gmail.com>
2023-07-31 17:35:40 +0900

    [SWM-280] Feat : vertical lecture card hover animation

--- src/components/recommendations/LectureRecommendationsCard.tsx
+++ src/components/recommendations/LectureRecommendationsCard.tsx
@@ -18,33 +18,35 @@ export default function LectureRecommendationsCard({ lecture }: Props) {
   } = lecture;
 
   return (
-    <VerticalSmallLectureCard src={thumbnail} alt={lecture_title}>
-      <div className='h-full'>
-        <div className='mb-1'>
-          <p className='text-lg font-bold whitespace-normal line-clamp-1'>
-            {lecture_title}
-          </p>
+    <div className='transition-all hover:-translate-y-2 hover:bg-zinc-100 hover:cursor-pointer'>
+      <VerticalSmallLectureCard src={thumbnail} alt={lecture_title}>
+        <div className='h-full'>
+          <div className='mb-1'>
+            <p className='text-lg font-bold whitespace-normal line-clamp-1'>
+              {lecture_title}
+            </p>
+          </div>
+          <div className='mb-1'>
+            <p className='text-sm font-semibold whitespace-normal text-zinc-700 line-clamp-1'>
+              {channel}
+            </p>
+          </div>
+          <div className='mb-1 min-h-[3.5rem]'>
+            <p className='text-sm whitespace-normal text-zinc-500 line-clamp-3'>
+              {description}
+            </p>
+          </div>
+          <StarRatingWithReviewCount
+            rating={rating}
+            review_count={review_count}
+          />
         </div>
-        <div className='mb-1'>
-          <p className='text-sm font-semibold whitespace-normal text-zinc-700 line-clamp-1'>
-            {channel}
-          </p>
-        </div>
-        <div className='mb-1 min-h-[3.5rem]'>
-          <p className='text-sm whitespace-normal text-zinc-500 line-clamp-3'>
-            {description}
-          </p>
-        </div>
-        <StarRatingWithReviewCount
-          rating={rating}
-          review_count={review_count}
-        />
-      </div>
-      {is_playlist && (
-        <div className='absolute top-0 left-0'>
-          <ThumbnailBadge title='재생목록' className='bg-orange-500' />
-        </div>
-      )}
-    </VerticalSmallLectureCard>
+        {is_playlist && (
+          <div className='absolute top-0 left-0'>
+            <ThumbnailBadge title='재생목록' className='bg-orange-500' />
+          </div>
+        )}
+      </VerticalSmallLectureCard>
+    </div>
   );
 }
