7f5b1b7386d0f363d75ab120ba0e450d548b50ac
oikkoikk <ljjun0702@gmail.com>
2023-08-08 23:42:57 +0900

    Fix : card overflow error

--- src/components/ui/lectureCard/HorizontalSmallLectureCard.tsx
+++ src/components/ui/lectureCard/HorizontalSmallLectureCard.tsx
@@ -12,11 +12,11 @@ export default function HorizontalSmallLectureCard({
   children
 }: Props) {
   return (
-    <div className='border-zinc-200 border relative w-[42rem] h-[11rem] bg-white flex p-3 gap-4 overflow-hidden'>
+    <div className='border-zinc-200 border relative w-[42rem] h-[11rem] bg-white flex p-3 gap-4 overflow-hidden whitespace-normal'>
       <div className='relative object-cover md:min-w-[calc(9rem*1.78)] md:max-w-[calc(9rem*1.78)]'>
         <Image fill={true} sizes='100%' src={src} alt={alt} />
       </div>
-      <div className='w-[calc(38.5rem-(9rem*1.78))] h-full'>{children}</div>
+      <div className='w-full h-full overflow-hidden whitespace-normal'>{children}</div>
     </div>
   );
 }
