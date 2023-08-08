fd71ead1787e1e7215e257acb0d5ad80880db586
oikkoikk <ljjun0702@gmail.com>
2023-08-08 23:54:18 +0900

    Fix : Horizontal big card overflow error

--- src/components/ui/lectureCard/HorizontalBigLectureCard.tsx
+++ src/components/ui/lectureCard/HorizontalBigLectureCard.tsx
@@ -12,11 +12,11 @@ export default function HorizontalBigLectureCard({
   children
 }: Props) {
   return (
-    <div className='relative flex w-full gap-5 p-3 overflow-hidden whitespace-normal bg-white lg:gap-8'>
+    <div className='relative flex w-full gap-5 p-3 whitespace-normal bg-white lg:gap-8'>
       <div className='relative object-cover min-w-[calc(7rem*1.78)] lg:min-w-[calc(12rem*1.78)] xl:min-w-[calc(19rem*1.78)] min-h-[7rem] lg:min-h-[12rem] xl:min-h-[19rem]'>
         <Image fill={true} sizes='100%' src={src} alt={alt} />
       </div>
-      <div className='w-full min-h-full overflow-hidden whitespace-normal'>
+      <div className='w-auto min-h-full whitespace-normal'>
         {children}
       </div>
     </div>
