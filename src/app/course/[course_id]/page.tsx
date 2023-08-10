458662b9a9c5848d10693e37f19d5f0244dff9ee
oikkoikk <ljjun0702@gmail.com>
2023-08-10 21:22:27 +0900

    [SWM-223] Feat : YoutubePlayer.tsx

--- src/app/course/[course_id]/page.tsx
+++ src/app/course/[course_id]/page.tsx
@@ -1,9 +1,20 @@
 import { fetchCourseDetail } from '@/src/api/courses/courses';
+import YoutubePlayer from '@/src/components/course/YoutubePlayer';
 
 export default async function CourseTakingPage({
   params: { course_id }
 }: CourseTakingPageParams) {
   const courseDetail = await fetchCourseDetail(parseInt(course_id));
+  const { last_view_video } = courseDetail;
 
-  return <>{courseDetail.course_title}</>;
+  return (
+    <>
+      <YoutubePlayer
+        video_code={last_view_video.video_code}
+        width={300 * 1.78}
+        height={300}
+        start={last_view_video.last_view_duration}
+      />
+    </>
+  );
 }
