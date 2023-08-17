20289bb0529387a5b48c1118e5cae4cf391c41a9
Lee, Jongjun <ljjun0702@gmail.com>
2023-08-17 18:25:45 +0900

    [SWM-185] Fix : ProgressBar width

--- src/components/dashboard/main/CorrectnessRate.tsx
+++ src/components/dashboard/main/CorrectnessRate.tsx
@@ -10,7 +10,7 @@ export default function CorrectnessRate({value}: Props) {
       <p className='text-xs font-semibold leading-8 sm:text-sm md:text-md: lg:text-lg xl:text-xl'>
         퀴즈 총 정답율 <span className='text-lg font-bold sm:text-2xl lg:text-3xl xl:text-4xl'>{value}</span>%
       </p>
-      <ProgressBar className='w-[78%] h-2 lg:h-[0.58rem]' value={80} />
+      <ProgressBar className='!w-[78%] h-2 lg:h-[0.58rem]' value={80} />
     </div>
   );
 }
