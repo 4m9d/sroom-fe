557222ae6156985ec0e1ff6e5f433931ee3bbfb0
Lee, Jongjun <ljjun0702@gmail.com>
2023-08-17 10:45:02 +0900

    [SWM-185] Design : MainDashboard - TotalLearningTime

--- src/components/dashboard/main/TotalLearningTime.tsx
+++ src/components/dashboard/main/TotalLearningTime.tsx
@@ -25,11 +25,11 @@ export default function TotalLearningTime({ time }: Props) {
   );
 
   return (
-    <div className='flex flex-col justify-between col-start-1 col-end-2 row-start-2 row-end-4 px-5 py-5 bg-zinc-100'>
-      <p className='text-sm'>누적 수강 시간</p>
-      <p className='font-bold text-7xl'>
+    <div className='flex flex-col justify-between col-start-1 col-end-2 row-start-2 row-end-4 px-[10.4%] py-[13%] bg-sroom-gray-300 text-sroom-black-400'>
+      <p className='text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl'>누적 수강 시간</p>
+      <p className='text-lg font-bold sm:text-4xl md:text-5xl lg:text-7xl xl:text-9xl'>
         {timeInString}
-        <span className='text-lg font-semibold'>{timeUnit}</span>
+        <span className='text-sm sm:text-lg md:text-xl lg:text-2xl xl:text-3xl'>{timeUnit}</span>
       </p>
     </div>
   );
