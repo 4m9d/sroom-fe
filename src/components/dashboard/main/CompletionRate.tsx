489043bfddc73a398d3ecde02842b157fdbe757c
Lee, Jongjun <ljjun0702@gmail.com>
2023-08-17 11:07:54 +0900

    [SWM-185] Design : MainDashboard - CompletionRate

--- src/components/dashboard/main/CompletionRate.tsx
+++ src/components/dashboard/main/CompletionRate.tsx
@@ -1,18 +1,20 @@
-import RadialProgress from "../../ui/RadialProgress";
+import RadialProgress from '../../ui/RadialProgress';
 
 type Props = {
   value: number;
-}
+};
 
-export default function CompletionRate({value}: Props) {
+export default function CompletionRate({ value }: Props) {
   return (
-    <div className='flex items-center justify-center col-start-2 col-end-3 row-start-3 row-end-4 gap-5 bg-orange-500'>
-      <p className='text-sm text-white'>등록한 강의 완강률</p>
-      <RadialProgress
-        className='text-white '
-        innerTextClassName='text-black text-2xl font-bold'
-        value={value}
-      />
+    <div className='flex items-center justify-center gap-5 bg-sroom-brand'>
+      <p className='text-xs font-semibold sm:text-sm md:text-md lg:text-lg xl:text-xl text-sroom-white'>등록한 강의 완강률</p>
+      <div className='h-[65.7%] w-[22.8%] flex justify-center items-center'>
+        <RadialProgress
+          className='w-full h-full text-sroom-white'
+          innerTextClassName='text-sroom-black-400 text-xs sm:text-sm md:text-md lg:text-xl xl:text-3xl font-bold'
+          value={value}
+        />
+      </div>
     </div>
   );
 }
--- src/components/ui/RadialProgress.tsx
+++ src/components/ui/RadialProgress.tsx
@@ -6,7 +6,7 @@ type Props = {
 };
 
 export default function RadialProgress({ className,innerTextClassName, value, size }: Props) {
-  const style = { '--value': 49, '--size': size } as React.CSSProperties;
+  const style = { '--value': value, '--size': size } as React.CSSProperties;
   return (
     <div className={`${className} radial-progress radial-square`} style={style}>
       <p className={`${innerTextClassName}`}>
