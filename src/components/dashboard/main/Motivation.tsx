018f99264c403e347711f9e816a00c03eccc5b99
Lee, Jongjun <ljjun0702@gmail.com>
2023-08-17 10:34:41 +0900

    [SWM-185] Design : MainDashboard - Motivation

--- src/components/dashboard/main/Motivation.tsx
+++ src/components/dashboard/main/Motivation.tsx
@@ -1,11 +1,11 @@
 type Props = {
   text: string;
-}
+};
 
-export default function Motivation({text}: Props) {
+export default function Motivation({ text }: Props) {
   return (
-    <div className='flex items-center justify-center col-start-1 col-end-3 row-start-1 row-end-2 bg-orange-500 rounded-full'>
-      <p className='text-lg font-semibold text-white'>
+    <div className='flex items-center col-start-1 col-end-3 row-start-1 row-end-2 px-[5%] rounded-full bg-sroom-brand'>
+      <p className='text-xs font-semibold whitespace-normal sm:text-sm md:text-base lg:text-lg xl:text-xl text-sroom-white line-clamp-2'>
         {text}
       </p>
     </div>
