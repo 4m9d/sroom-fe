240f1fdc68b4b74f774f80c68837b41da8e91c74
Lee, Jongjun <ljjun0702@gmail.com>
2023-08-17 10:33:20 +0900

    [SWM-185] Design : Dashboard Header

--- src/components/dashboard/header/GuideBox.tsx
+++ src/components/dashboard/header/GuideBox.tsx
@@ -1,16 +1,22 @@
 export default function GuideBox({
   title,
-  description
+  description,
+  children
 }: {
   title: string;
   description: string;
+  children?: React.ReactNode;
 }) {
   return (
-    <div className='flex-1 h-24 hover:cursor-pointer hover:h-40 px-4 py-3 transition-all bg-white hover:bg-black hover:text-white shadow-[0_-2px_5px_1px_rgba(173,173,173,0.3)]'>
-      <p className='mb-1 text-xs font-semibold'>{title}</p>
-      <p className='text-base font-bold'>
-        {description} <span className='ml-2'>〉</span>
-      </p>
+    <div className='relative flex-1 h-[9.5rem] lg:h-40 hover:cursor-pointer lg:hover:h-[17rem] p-4 lg:p-7 transition-all bg-white hover:bg-gradient-to-b from-[#222222] to-sroom-black-300 hover:text-white shadow-[0_-2px_5px_1px_rgba(173,173,173,0.3)] text-sroom-black-400 group'>
+      <p className='mb-1 text-xs font-normal lg:text-lg'>{title}</p>
+      <div className='flex items-center gap-2'>
+        <p className='text-base font-bold lg:text-2xl'>{description}</p>
+        <p className='text-sm font-bold lg:text-base'>〉</p>
+      </div>
+      <div>
+        {children}
+      </div>
     </div>
   );
 }
