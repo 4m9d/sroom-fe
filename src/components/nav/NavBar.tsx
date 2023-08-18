eefe45a510e8ebfc8a9840584c12d0fe0f40e002
Lee, Jongjun <ljjun0702@gmail.com>
2023-08-18 15:36:36 +0900

    [SWM-186] Fix : NavBar layout

--- src/components/nav/NavBar.tsx
+++ src/components/nav/NavBar.tsx
@@ -33,35 +33,38 @@ export default function NavBar({ logo, profileDropdown }: Props) {
       <div className={`${hidden} flex-1`}>
         <SearchInput />
       </div>
-      <div className='flex gap-4 w-72'>
-        <Button onClick={logout} className={`${hidden} g_id_signout w-24 bg-sroom-brand`}>
-          <p className='text-sm font-semibold text-sroom-white'>로그아웃</p>
-        </Button>
-        <button
-          type='button'
-          className={`${hidden} dropdown dropdown-hover w-44 text-sroom-black-400`}
+      <Button
+        onClick={logout}
+        className={`${hidden} g_id_signout w-24 bg-sroom-brand`}
+      >
+        <p className='text-sm font-semibold text-sroom-white'>로그아웃</p>
+      </Button>
+      <button
+        type='button'
+        className={`${hidden} dropdown dropdown-hover w-44 text-sroom-black-400`}
+      >
+        <div
+          tabIndex={0}
+          className='flex flex-col items-start justify-between h-full rounded-none w-52 btn btn-ghost hover:bg-sroom-gray-300'
         >
-          <div
-            tabIndex={0}
-            className='flex flex-col items-start justify-between h-full rounded-none w-52 btn btn-ghost hover:bg-sroom-gray-300'
-          >
-            <p className='text-sm font-semibold text-left'>{name}</p>
-            <p className='text-xs font-normal text-left whitespace-normal text-sroom-black-100 line-clamp-1'>{bio}</p>
-          </div>
-          <ul
-            tabIndex={0}
-            className='menu dropdown-content z-[1] p-2 w-52 shadow rounded-none'
-          >
-            {profileDropdown?.map((menu) => {
-              return (
-                <li key={menu.id}>
-                  <Link href={menu.menuRoute}>{menu.menuTitle}</Link>
-                </li>
-              );
-            })}
-          </ul>
-        </button>
-      </div>
+          <p className='text-sm font-semibold text-left'>{name}</p>
+          <p className='text-xs font-normal text-left whitespace-normal text-sroom-black-100 line-clamp-1'>
+            {bio}
+          </p>
+        </div>
+        <ul
+          tabIndex={0}
+          className='menu dropdown-content z-[1] p-2 w-52 shadow rounded-none'
+        >
+          {profileDropdown?.map((menu) => {
+            return (
+              <li key={menu.id}>
+                <Link href={menu.menuRoute}>{menu.menuTitle}</Link>
+              </li>
+            );
+          })}
+        </ul>
+      </button>
     </nav>
   );
 }
