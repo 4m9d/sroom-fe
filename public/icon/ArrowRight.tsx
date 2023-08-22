576b8d6fcb6ad3975f8252160c2e5af41b6f8fb1
Lee, Jongjun <ljjun0702@gmail.com>
2023-08-22 21:35:39 +0900

    [SWM-187] Install : svgr

---
+++ public/icon/ArrowRight.tsx
@@ -0,0 +1,19 @@
+export default function ArrowRightSVG() {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='current'
+      height='current'
+      viewBox='0 0 12 12'
+      fill='none'
+    >
+      <path
+        d='M4.75736 1.75736L9 6L4.75736 10.2426'
+        stroke='current'
+        stroke-width='1.5'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+      />
+    </svg>
+  );
+}
---
+++ public/icon/Lecture.tsx
@@ -0,0 +1,21 @@
+export default function LectureSVG() {
+  return (
+    <svg
+      width='current'
+      height='current'
+      viewBox='0 0 12 12'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path d='M8 6.00002L4.5 8.16509L4.5 3.83496L8 6.00002Z' fill='current' />
+      <rect
+        x='0.5'
+        y='1.5'
+        width='11'
+        height='9'
+        stroke='current'
+        stroke-linejoin='round'
+      />
+    </svg>
+  );
+}
