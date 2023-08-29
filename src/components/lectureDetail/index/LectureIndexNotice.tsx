d912ce630cead4fe2e0ba4dbd96b75ef1bb5ecb6
Lee, Jongjun <ljjun0702@gmail.com>
2023-08-29 10:33:02 +0900

    [SWM-260] Fix : hasMembersOnly logic

--- src/components/lectureDetail/index/LectureIndexNotice.tsx
+++ src/components/lectureDetail/index/LectureIndexNotice.tsx
@@ -34,17 +34,19 @@ export default function LectureIndexNotice({
           </div>
         </div>
       </div>
-      <div className='flex items-center justify-start w-full gap-5 px-5 py-5 mb-10 text-xs font-medium rounded-sm md:gap-10 md:px-10 md:text-sm min-h-[3.5rem] max-h-[5rem] bg-sroom-gray-200 border border-sroom-gray-400 text-sroom-black-400'>
-        <div className='text-base md:text-xl'>👑</div>
-        <div className='flex flex-col w-full gap-1'>
-          <div className='text-base font-bold md:text-lg'>
-            회원 전용 강의가 포함되어 있어요!
-          </div>
-          <div className='text-sroom-black-300'>
-            해당 채널의 멤버십에 가입하시면 모든 강의를 시청하실 수 있어요.
+      {hasMembersOnly && (
+        <div className='flex items-center justify-start w-full gap-5 px-5 py-5 mb-10 text-xs font-medium rounded-sm md:gap-10 md:px-10 md:text-sm min-h-[3.5rem] max-h-[5rem] bg-sroom-gray-200 border border-sroom-gray-400 text-sroom-black-400'>
+          <div className='text-base md:text-xl'>👑</div>
+          <div className='flex flex-col w-full gap-1'>
+            <div className='text-base font-bold md:text-lg'>
+              회원 전용 강의가 포함되어 있어요!
+            </div>
+            <div className='text-sroom-black-300'>
+              해당 채널의 멤버십에 가입하시면 모든 강의를 시청하실 수 있어요.
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </>
   );
 }
