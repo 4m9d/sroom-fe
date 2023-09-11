990252dc5a4c4a31c1bd79c22628140c1215823b
Lee, Jongjun <ljjun0702@gmail.com>
2023-09-11 20:50:58 +0900

    [SWM-290] Feat : quiz grading

--- src/components/course/drawer/courseMaterial/quizzes/CourseMaterialQuizzes.tsx
+++ src/components/course/drawer/courseMaterial/quizzes/CourseMaterialQuizzes.tsx
@@ -1,4 +1,5 @@
 'use client';
+import Button from '@/src/components/ui/button/Button';
 import QuizCard from '@/src/components/ui/quizCard/QuizCard';
 import { SessionStorageKeys } from '@/src/constants/materials/materials';
 import { AnimatePresence, motion } from 'framer-motion';
@@ -20,6 +21,33 @@ export default function CourseMaterialQuizzes({
     previouslySelectedAnswerList ? JSON.parse(previouslySelectedAnswerList) : []
   );
 
+  const isSubmitButtonDisabled =
+    selectedAnswerList.filter((answer) => answer.submitted_answer !== '')
+      .length !== quizzes.length || selectedAnswerList[0].is_submitted === true;
+
+  const gradeSelectedQuizzes = () => {
+    const gradedQuizzes = selectedAnswerList.map((selectedAnswer) => {
+      const quiz = quizzes.find((quiz) => quiz.id === selectedAnswer.id);
+      if (!quiz) return;
+
+      const isCorrect = quiz.answer === selectedAnswer.submitted_answer;
+
+      return {
+        ...selectedAnswer,
+        is_correct: isCorrect,
+        is_submitted: true
+      };
+    });
+    return gradedQuizzes as SelectedQuizAnswer[];
+  };
+
+  const quizSubmitHandler = () => {
+    const gradedQuizzes = gradeSelectedQuizzes();
+    setSelectedAnswerList(() => gradedQuizzes);
+
+    console.log(selectedAnswerList);
+  };
+
   useEffect(() => {
     sessionStorage.setItem(
       sessionStorageKey,
@@ -33,6 +61,9 @@ export default function CourseMaterialQuizzes({
         className={`w-full my-10 flex flex-col justify-center items-between gap-10`}
       >
         {quizzes.map((quiz, idx) => {
+          const selectedAnswer = selectedAnswerList.find(
+            (answer) => answer.id === quiz.id
+          );
           return (
             <motion.section
               initial={{ opacity: 0, y: 10 }}
@@ -43,12 +74,23 @@ export default function CourseMaterialQuizzes({
               <QuizCard
                 selectedAnswerList={selectedAnswerList}
                 setSelectedAnswerList={setSelectedAnswerList}
+                isCorrect={selectedAnswer?.is_correct}
+                isScrapped={selectedAnswer?.is_scrapped}
                 quiz={quiz}
                 questionNumber={idx + 1}
               />
             </motion.section>
           );
         })}
+        <Button
+          onClick={quizSubmitHandler}
+          disabled={isSubmitButtonDisabled}
+          className={`w-full mt-2 border py-7 border-sroom-black-400 bg-sroom-white ${
+            isSubmitButtonDisabled ? 'opacity-50' : ''
+          }`}
+        >
+          {'제출하기'}
+        </Button>
       </div>
     </AnimatePresence>
   );
