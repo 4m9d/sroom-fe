ebeed564fadea24689ab0b726afffe67f4521b14
Lee, Jongjun <ljjun0702@gmail.com>
2023-09-16 00:10:11 +0900

    [SWM-293] Fix : Enrollment button layout

--- src/components/lectureEnrollment/LectureEnrollmentButton.tsx
+++ src/components/lectureEnrollment/LectureEnrollmentButton.tsx
@@ -173,7 +173,7 @@ export default function LectureEnrollmentButton({
           <button
             type='button'
             onClick={() => mutate(undefined)}
-            className='flex items-center justify-center gap-2 hover:text-sroom-white'
+            className='flex items-center justify-center w-full gap-2 hover:text-sroom-white'
           >
             <div className='flex items-center justify-center w-5 h-5 rounded-full shrink-0 bg-sroom-black-400 text-sroom-white'>
               +
