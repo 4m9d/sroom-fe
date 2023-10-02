33f09b759854a03b0fc1aef5107f7f51ddbd5cab
oikkoikk <ljjun0702@gmail.com>
2023-10-02 13:06:25 +0900

    [SWM-317] Feat : 404 not found page

--- src/app/api/health/health.ts
+++ src/app/api/health/health.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function healthCheckHandler(
-  req: NextApiRequest,
+  _req: NextApiRequest,
   res: NextApiResponse
 ) {
   res.status(200).json({ status: 'ok' });
