bc61e54fe78d67d3afd9fc751b4b7892e88cf5c7
oikkoikk <ljjun0702@gmail.com>
2023-10-13 02:04:02 +0900

    [SWM-344] Feat : ServiceGuideModal

--- src/components/dashboard/header/GuideBox.tsx
+++ src/components/dashboard/header/GuideBox.tsx
@@ -3,14 +3,16 @@ import ArrowRightSVG from '@/public/icon/ArrowRight';
 export default function GuideBox({
   title,
   description,
+  onClick,
   children
 }: {
   title: string;
   description: string;
+  onClick?: () => void;
   children?: React.ReactNode;
 }) {
   return (
-    <div className='relative flex-1 h-[9.5rem] lg:h-40 hover:cursor-pointer lg:hover:h-[17rem] p-4 lg:p-7 transition-all bg-white hover:bg-gradient-to-b from-[#222222] to-sroom-black-300 hover:text-white shadow-[0_-2px_5px_1px_rgba(173,173,173,0.3)] text-sroom-black-400 group'>
+    <button type='button' onClick={onClick} className='flex flex-col relative flex-1 h-[9.5rem] lg:h-40 hover:cursor-pointer lg:hover:h-[17rem] p-4 lg:p-7 transition-all bg-white hover:bg-gradient-to-b from-[#222222] to-sroom-black-300 hover:text-white shadow-[0_-2px_5px_1px_rgba(173,173,173,0.3)] text-sroom-black-400 group'>
       <p className='mb-1 text-xs font-normal lg:text-lg'>{title}</p>
       <div className='flex items-center gap-2'>
         <p className='text-base font-bold lg:text-2xl'>{description}</p>
@@ -19,6 +21,6 @@ export default function GuideBox({
         </span>
       </div>
       <div>{children}</div>
-    </div>
+    </button>
   );
 }
---
+++ src/constants/serviceGuides/serviceGuides.ts
@@ -0,0 +1,17 @@
+export const SERVICE_GUIDE = [
+  {
+    id: 1,
+    type: 'SEARCH',
+    key: 'd655bb34f8b2448f9a00e1212f6fe39f'
+  },
+  {
+    id: 2,
+    type: 'SCHEDULING',
+    key: '4ad5a4933fb54f1eb10a73278b354acb'
+  },
+  {
+    id: 3,
+    type: 'MATERIALS',
+    key: '43871bb7c7ca4a69b9836728aa22efb6'
+  }
+] as const;
