e273efd41d5bbd251dca8781a0328bfdf6e16dbe
oikkoikk <ljjun0702@gmail.com>
2023-10-30 12:53:55 +0900

    [SWM-369] Fix : section heading link boundary

--- src/components/ui/SectionHeading.tsx
+++ src/components/ui/SectionHeading.tsx
@@ -9,10 +9,13 @@ type Props = {
 export default function SectionHeading({ title, children, onClick }: Props) {
   return (
     <div
-      onClick={onClick}
-      className={`${onClick ? 'hover:cursor-pointer' : ''} flex items-center mb-5 text-2xl font-bold bg-transparent md:mb-10 md:text-3xl text-sroom-black-400`}
+      className={`${
+        onClick ? 'hover:cursor-pointer' : ''
+      } flex items-center mb-5 text-2xl font-bold bg-transparent md:mb-10 md:text-3xl text-sroom-black-400`}
     >
-      <h2>{title}</h2>
+      <div onClick={onClick}>
+        <h2>{title}</h2>
+      </div>
       {onClick && (
         <span className='w-5 h-5 md:w-6 md:h-6 stroke-sroom-black-400'>
           <ArrowRightSVG />
